66e489d3cab8f865b285b553f55e3f5c62d3fc8a
Kimball <206552940@TFAYD.com>
2021-10-17 15:56:12 -0400

    payloads

---
+++ payloads_bls.R
@@ -0,0 +1,146 @@
+list('seriesid' = c(), 'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_asian_ind <- list('seriesid' = c("LNU02073393", "LNU02072743", "LNU02072751", "LNU02072758", 
+                                         "LNU02072759", "LNU02072859", "LNU02072883", "LNU02072921", "LNU02072934",
+                                         "LNU02072941", "LNU02072960", "LNU02072975", "LNU02072999", "LNU02073004", 
+                                         "LNU02073022" "LNU02073035" "LNU02073057"), 
+                          'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_asian_reason_unemp <- list('seriesid' = c("LNU03034295", "LNU03034296", "LNU03034297", "LNU03034298", "LNU03034299", "LNU03034300"), 
+                                   'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_black_ind <- list('seriesid' = c("LNU02073392", "LNU02072419", "LNU02072427", "LNU02072434", "LNU02072435", "LNU02072535", 
+                                         "LNU02072559", "LNU02072597", "LNU02072610", "LNU02072617", "LNU02072636", "LNU02072651", 
+                                         "LNU02072675", "LNU02072680", "LNU02072698", "LNU02072711", "LNU02072733"), 
+                          'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_black_reason_unemp <- list('seriesid' = c("LNU03025396", "LNU03017273", "LNU03017279", "LNU03017285","LNU03017234", "LNU03017240"), 
+                                  'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_discouraged <- list('seriesid' = c("LNU05000000", "LNU05027039", "LNU05026639", "LNU05027045", "LNU05027496", "LNU05027054", 
+                                           "LNU05026642", "LNU05026645", "LNU05026648", "LNU05027069", "LNU05027075", "LNU05027081", 
+                                           "LNU05027695"), 
+                            'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_education_genderrace <- list('seriesid' = c("LNU04000052", "LNU04092181", "LNU04092189", "LNU04092205", "LNU04092228", 
+                                                    "LNU04091120", "LNU04000053", "LNU04092182", "LNU04092190","LNU04092206", 
+                                                    "LNU04092233", "LNU04091121", "LNU04000055", "LNU04092183", "LNU04092191", 
+                                                    "LNU04092207", "LNU04092229", "LNU04091122", "LNU04000056", "LNU04092184", 
+                                                    "LNU04092192", "LNU04092208", "LNU04092234", "LNU04091123", "LNU04032324", 
+                                                    "LNU04092185", "LNU04092193", "LNU04092209", "LNU04092230", "LNU04091124", 
+                                                    "LNU04032365", "LNU04092186", "LNU04092194", "LNU04092210", "LNU04092235", 
+                                                    "LNU04091125","LNU04000058", "LNU04092187", "LNU04092195", "LNU04092211", 
+                                                    "LNU04092227", "LNU04091126", "LNU04000059", "LNU04092188", "LNU04092196",
+                                                    "LNU04092212", "LNU04092232", "LNU04091127"), 
+                                     'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_education_unemp <- list('seriesid' = c( "LNU04000048", "LNU04027659", "LNU04027660", "LNU04027683", "LNU04092221", 
+                                                "LNU04091113", "LNU04000049", "LNU04027675", "LNU04027676", "LNU04027687", 
+                                                "LNU04092226", "LNU04091114", "LNU04000050", "LNU04027679", "LNU04027680", 
+                                                "LNU04027688", "LNU04092231", "LNU04091115", "LNU04000051", "LNU04027667", 
+                                                "LNU04027668", "LNU04027685", "LNU04092223", "LNU04091116", "LNU04000054", 
+                                                "LNU04027671", "LNU04027672", "LNU04027686", "LNU04092224", "LNU04091117", 
+                                                "LNU04027663", "LNU04027664", "LNU04027684", "LNU04092222", "LNU04091119", 
+                                                "LNU04034985", "LNU04032295", "LNU04032296", "LNU04032299", "LNU04092225", "LNU04091118"), 
+                                'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_emp_level <- list('seriesid' = c("LNS12000000", "LNS12000001", "LNS12000002", "LNS12000003", "LNS12000006", "LNS12000009", 
+                                         "LNS12032183", "LNS12000028", "LNS12000029","LNS12000031", "LNS12000032", "LNS12000034", 
+                                         "LNS12000035", "LNU02032266", "LNU02032267"), 
+                          'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_emp_pop_ratio <- list('seriesid' = c("LNS12300000", "LNS12300001", "LNS12300002", "LNS12300003", "LNS12300006", "LNS12300009",
+                                             "LNU02332183", "LNS12300028", "LNS12300029", "LNS12300031", "LNS12300032", "LNS12300034", 
+                                             "LNS12300035"), 
+                              'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_hispanic_ind <- list('seriesid' = c( "LNU02073394", "LNU02073067", "LNU02073075", "LNU02073082", "LNU02073083", "LNU02073183"
+                                             "LNU02073207", "LNU02073245", "LNU02073258","LNU02073265", "LNU02073284", "LNU02073299",
+                                             "LNU02073323", "LNU02073328", "LNU02073346", "LNU02073359" ,"LNU02073381"), 
+                             'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_hispanic_reason_unemp <- list('seriesid' = c("LNU03023751", "LNU03023752", "LNU03023753", "LNU03023754", "LNU03023755", "LNU03023756"),
+                                      'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_ind_hourly_earnings <- list('seriesid' = c("CES0500000003", "CES1000000003", "CES2000000003", "CES3000000003", "CES4142000003", 
+                                                   "CES4200000003", "CES4300000003", "CES4422000003", "CES5000000003", "CES5500000003", 
+                                                   "CES6000000003", "CES6500000003", "CES7000000003", "CES8000000003"), 
+                                    'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_labor_force_level <- list('seriesid' = c("LNS11000000", "LNS11000001", "LNS11000002", "LNS11000003" ,"LNS11000006", 
+                                                 "LNS11000009", "LNS11032183", "LNS11000028", "LNS11000029", "LNS11000031", 
+                                                 "LNS11000032", "LNS11000034", "LNS11000035"), 
+                                  'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_labor_force_particip <- list('seriesid' = c("LNS11300000", "LNS11300001", "LNS11300002", "LNS11300003", "LNS11300006", 
+                                                    "LNS11300009", "LNS11332183", "LNS11300028", "LNS11300029", "LNS11300031", 
+                                                    "LNS11300032", "LNS11300034", "LNS11300035", "LNU01332266" ,"LNU01332267"), 
+                                     'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_reason_pct <- list('seriesid' = c("LNS13023622", "LNS13023654", "LNS13026511", "LNS13023706", "LNS13023558", "LNS13023570"), 
+                           'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_reason_unemp <- list('seriesid' = c("LNS13000000", "LNS13023621", "LNS13023653", "LNS13025699", "LNS13026638", "LNS13026637", 
+                                            "LNS13023705", "LNS13023557", "LNS13023569"), 
+                             'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_unemp_duration <- list('seriesid' = c("LNU03008275", "LNU03008307", "LNU03017227", "LNU03034306", "LNU03022433", "LNU03008311",
+                                              "LNU03008349", "LNU03017229", "LNU03034313", "LNU03034327", "LNU03008354", "LNU03008392",
+                                              "LNU03017231", "LNU03034320", "LNU03034334", "LNU03008275"), 
+                               'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_unemp_level <- list('seriesid' = c("LNS13000000", "LNS13000001", "LNS13000002", "LNS13000003", "LNS13000006", "LNS13000009",
+                                           "LNS13032183", "LNS13000028", "LNS13000029", "LNS13000031", "LNS13000032", "LNU03000034", 
+                                           "LNU03000035"), 
+                            'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_unemp_rate <- list('seriesid' = c("LNS14000000", "LNS14000001", "LNS14000002", "LNS14000003", "LNS14000006", "LNS14000009", 
+                                          "LNS14032183", "LNS14000028", "LNS14000029", "LNS14000031", "LNS14000032", "LNS14000034", 
+                                          "LNS14000035", "LNU04032266", "LNU04032267"), 
+                           'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_unemp_rate_ind <- list('seriesid' = c("LNU04000000", "LNU04035109", "LNU04032229", "LNU04032230", "LNU04032231", "LNU04032232",
+                                              "LNU04034154", "LNU04034163", "LNU04034168", "LNU04034171", "LNU04032237", "LNU04032238", 
+                                              "LNU04032239", "LNU04034236", "LNU04034239", "LNU04032241", "LNU04032242", "LNU04028615",
+                                              "LNU04035181"), 
+                               'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_weekly_earnings <- list('seriesid' = c("LEU0252881500", "LEU0252881800", "LEU0252882700", "LEU0252883600", "LEU0252883900", 
+                                               "LEU0252884200", "LEU0252884500", "LEU0252884800", "LEU0252885100", "LEU0252885400", 
+                                               "LEU0252885700", "LEU0252886000", "LEU0254468400", "LEU0254468500", "LEU0254468600"),
+                                'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_white_ind <- list('seriesid' = c("LNU02081921", "LNU02094783", "LNU02094792", "LNU02094806", "LNU02094851", "LNU02094907", 
+                                         "LNU02094931", "LNU02094969", "LNU02094799", "LNU02094982", "LNU02095002", "LNU02095017",
+                                         "LNU02095041", "LNU02095046", "LNU02095064", "LNU02095077", "LNU02095099"), 
+                          'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_white_reason_unemp <- list('seriesid' = c("LNU03023626", "LNU03023658", "LNU03026518", "LNU03023710", "LNU03023562", 
+                                                  "LNU03023574"), 
+                                   'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_women_uemp_ind <- list('seriesid' = c("LNU02073391", "LNU02072095", "LNU02072103", "LNU02072110", "LNU02072111", "LNU02072211",
+                                              "LNU02072235", "LNU02072273", "LNU02072286", "LNU02072293", "LNU02072312",
+                                              "LNU02072327", "LNU02072351", "LNU02072356", "LNU02072374", "LNU02072387", 
+                                              "LNU02072409"), 
+                               'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+payload_youth_unemp_rate <- list('seriesid' = c("LNS14000012", "LNS14000013", "LNS14000014", "LNS14000015", "LNS14000016", 
+                                                "LNS14000017", "LNS14000018", "LNS14000019", "LNS14000020", "LNU04000012", 
+                                                "LNU04000013", "LNU04000014", "LNU04000015", "LNU04000016", "LNU04000017", 
+                                                "LNU04000018", "LNU04000019", "LNU04000020", "LNU04032245", "LNU04032307", 
+                                                "LNU04032348", "LNU04000021", "LNU04000022", "LNU04000023"), 
+                                 'startyear' = 2019, 'endyear' = 2021, 'calculations' = TRUE, 'registrationKey' = registrationKey)
+
+
+
+
+
+
+
+
+
+
+
+
+
